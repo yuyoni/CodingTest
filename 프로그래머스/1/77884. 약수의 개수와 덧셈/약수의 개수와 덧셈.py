d48315785f57e521abfb9dfd20b5b15dd3f943d7
YU YEON LEE <127701092+yuyoni@users.noreply.github.com>
2023-10-14 20:19:23 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.40 ms, Memory: 9.93 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,19 @@
+def divisor(num):
+    cnt = 0
+    for i in range(1, num+1):
+        if num % i == 0 :
+            cnt += 1
+    return cnt
+
+
+def solution(left, right):
+    # 초기화
+    answer = 0
+    
+    for i in range(left, right+1):
+        if divisor(i) % 2 == 0 :
+            answer += i
+        else :
+            answer -= i
+            
+    return answer
