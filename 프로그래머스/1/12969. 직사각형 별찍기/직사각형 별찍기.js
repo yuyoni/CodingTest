965d6455d6e7ffba04ff515db4d8759885468c9f
YU YEON LEE <127701092+yuyoni@users.noreply.github.com>
2023-10-16 18:48:00 +0900

    [level 1] Title: 직사각형 별찍기, Time: 47.91 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,5 +3,4 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
     console.log(("*".repeat(a)+"\n").repeat(b));
-    console.log(b);
 });
