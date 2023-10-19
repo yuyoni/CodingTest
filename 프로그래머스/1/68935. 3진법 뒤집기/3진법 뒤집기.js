f59faa7af1b7e47dd2e743144229be32c803f98f
YU YEON LEE <127701092+yuyoni@users.noreply.github.com>
2023-10-20 00:28:56 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    let arr = n.toString(3).split('');
-    console.log(arr);
-    arr.reverse();
-    
-    
-    
-    return (parseInt(arr.join(''), 3));
+    return parseInt([...n.toString(3)].reverse().join(''), 3);
 }
