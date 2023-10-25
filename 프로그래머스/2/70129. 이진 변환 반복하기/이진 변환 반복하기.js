9a17a7d8ebd9c5f2ec70f4e761e9a6229e3ee95c
YU YEON LEE <127701092+yuyoni@users.noreply.github.com>
2023-10-25 18:30:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 30.19 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,9 +1,10 @@
 function solution(s) {
     let count = 0;
     let zero = 0;
-    
+    console.log(s.match(/0/g));
     while (s!=1){
-        zero += [...s].reduce((a,c)=> c == '0'? a+1: a, 0);
+        let tmpZero = s.match(/0/g) ? s.match(/0/g).length : 0;
+        zero += tmpZero;
         s = s.replace(/0/g, '');
         s = s.length.toString(2);
         count++;
