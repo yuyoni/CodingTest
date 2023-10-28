a2983601c2550b0828be0d799b343b6d68eae54d
YU YEON LEE <127701092+yuyoni@users.noreply.github.com>
2023-10-28 15:49:09 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,5 +1,5 @@
 function solution(n) {
-    for (num = n+1; num< 1000000; num++){
+    for (let num = n+1; num< 1000000; num++){
         if(n.toString(2).match(/1/g).length == num.toString(2).match(/1/g).length) return num;
     }
 }
