06b408c61ca0a66c5fd7ab4f2eeb0368e363506e
YU YEON LEE <127701092+yuyoni@users.noreply.github.com>
2023-10-28 17:39:06 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -6,7 +6,6 @@ function solution(n, words) {
     for(let i=0; i<words.length -1; i++) {
         if (words[i][words[i].length - 1] != words[i+1][0]) {
             theWord = words[i+1];
-            console.log(theWord);
             break;
         }
     }
@@ -16,7 +15,6 @@ function solution(n, words) {
     for (let i=0; i<words.length; i++) {
         if(answer.includes(words[i])) {
             theWord = words[i];
-            console.log(theWord);
             break;
         }
         else {
@@ -24,7 +22,9 @@ function solution(n, words) {
         }
     }
     
+    // 3번째 : 잘 되고있으므로 [0, 0] 반환
     if (theWord == '') return [0, 0]
+    
     
     // 탈락하는 사람의 번호 = (전체 길이 / n ) % words.lastIndexOf(theWord)
     // 그 사람이 자신의 몇 번째 차례에 탈락하는지 = Math.floor(words.lastIndexOf(theWord) / n)
@@ -32,8 +32,6 @@ function solution(n, words) {
     let turn = Math.floor(words.lastIndexOf(theWord) / n) + 1;
     
     answer = [num, turn];
-    console.log(answer);
-    
     
     return answer;
 }
