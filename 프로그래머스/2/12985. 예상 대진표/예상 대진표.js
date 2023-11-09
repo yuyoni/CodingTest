a595ced41c06b9b0ddc085d73fbf4e3a2ce21c72
YU YEON LEE <127701092+yuyoni@users.noreply.github.com>
2023-11-09 14:39:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b)
+{
+    let count = 0;
+    
+    while (a != b) {
+        a = Math.round(a / 2);
+        b = Math.round(b / 2);
+        count++;
+    }
+    
+    return count;
+}
